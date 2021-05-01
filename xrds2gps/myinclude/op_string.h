8081c92e0a1026b1b3fdc5b33e378c2e33bd3a25
Wael Hammoudeh <w_hammoudeh@hotmail.com>
2021-05-01 06:36:17 -0700

    modified parseGPS() func in op_string.c to store all found nodes

--- xrds2gps/myinclude/op_string.h
+++ xrds2gps/myinclude/op_string.h
@@ -19,10 +19,12 @@ int parseCurlXrdsData (XROADS *xrds, void *data);
 
 int parseWgetXrdsFile (XROADS *dst, void *filename);
 
-int parseGPS(XROADS *dst, char *str);
-
 int parseXrdsResult (XROADS *dstXrds, DL_LIST *srcDL);
 
 int response2LineDL (DL_LIST *dstDL, char *response);
+
+int parseGPS (GPS_PT *dst, char *str);
+
+void printXrds (XROADS *xrds);
 
 #endif /* OP_STRING_H_ */
